e5581e63db5dc26d43cb6a3028c628e9738e34a3
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2026-03-24 20:33:17 -0400

    finished layout of basic functions for core library

--- core/chunk.hpp
+++ core/chunk.hpp
@@ -1,4 +1,16 @@
+#pragma once
 
+#include "table.hpp"
+
+// Degree of the b+ tree
+const unsigned int CHUNK_SIZE = 10;
+ 
 class Chunk {
-  
+  private:
+    unsigned int index;
+    double data[CHUNK_SIZE];
+
+  public:
+    Chunk(): index(0) {};
+    friend class Table;
 };
--- core/table.hpp
+++ core/table.hpp
@@ -1,10 +1,31 @@
+#pragma once
 
 #include <string>
+#include <vector>
+
+#include "chunk.hpp"
 
 class Table {
   private:
 
-    const unsigned int CHUNK_SIZE = 10;
+    unsigned int num_attributes;
+    std::vector<std::string> attributes;
+    Chunk* root;
 
-    Chunk* root = new Chunk();
-}
+    unsigned int min_index;
+    unsigned int max_index;
+
+  public:
+    Table();
+
+    bool insert(std::string attribute_name, unsigned int index, double val);
+    bool remove(std::string attribute_name, unsigned int index, double val);
+
+    bool addAttribute(std::string name);
+    bool removeAttribute(std::string name);
+
+    void save(std::string path);
+    void load(std::string path);
+
+    bool apply(std::string attribute_name, std::string equation);
+};
