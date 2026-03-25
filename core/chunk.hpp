8316ff4760abe35a3d8c558611166b735e579f50
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2026-03-24 21:23:50 -0400

    added vars to chunk

--- core/chunk.hpp
+++ core/chunk.hpp
@@ -7,10 +7,14 @@ const unsigned int CHUNK_SIZE = 10;
  
 class Chunk {
   private:
+
+    bool is_leaf;
     unsigned int index;
+
     double data[CHUNK_SIZE];
+    Chunk* pointers[CHUNK_SIZE + 1];
 
   public:
-    Chunk(): index(0) {};
+    Chunk(): index(0), is_leaf(false) {};
     friend class Table;
 };
