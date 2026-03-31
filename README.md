793516d551f8e2d932529a1d9627eb9f43d5b9c2
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2026-03-31 10:06:28 -0400

    seperated leaf node and internal node:

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ A very heavily optimized efficient library for quantitative calculations and sto
 - https://docs.pola.rs/user-guide/getting-started/#installing-polars
 - https://duckdb.org
 
+# To-D0
+1. Write basic chunk functionality for internal and leaf nodes
+2. Test chunk functionality and make abstract easy interface to work with it
+3. Integrate chunk with table and make basic splittign functionality
+
 
 
 
--- core/chunk.hpp
+++ core/chunk.hpp
@@ -4,18 +4,25 @@
 
 // Degree of the b+ tree
 const unsigned int CHUNK_SIZE = 10;
- 
+
 class Chunk {
+  virtual bool isLeaf() const = 0;
+  friend class Table;
+};
+
+class LeafChunk: Chunk {
+  bool isLeaf() const; // returns true
+  double keys[CHUNK_SIZE];
+  Node* pointers[CHUNK_SIZE + 1];
+};
+ 
+class InternalChunk {
   private:
-
-    bool is_leaf;
-
-    unsigned int index;
     unsigned int num_attributes;
     unsigned int filled_data;
 
-    double data[CHUNK_SIZE];
-    Chunk* pointers[CHUNK_SIZE + 1];
+    double keys[CHUNK_SIZE];
+    Node* pointers[CHUNK_SIZE + 1];
 
   public:
     Chunk(unsigned int num_attributes): is_leaf(false), index(0), num_attributes(num_attributes), filled_data(0) {};
