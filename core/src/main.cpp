73dbabb93cf7cdf5214ea5ef2d3c2d5740cd254c
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2026-04-05 01:51:10 -0400

    got makefile working properly.

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,44 +1,24 @@
-// #define CATCH_CONFIG_MAIN
-// #include <iostream>
+#include <iostream>
 
-// #include "catch_amalgamated.hpp"
-// #include "chunks/chunk.hpp"
+#include "catch_amalgamated.hpp"
+#include "chunks/chunk.hpp"
 
-// TEST_CASE("Testing Leaf Chunk Insertion", "[leaf_chunk]") {
-//   LeafChunk test(5); // Should have 5 attributes
-//   test.insert(1, {1, 2, 3, 4, 5});
-//   test.insert(2, {3, 2, 3, 4, 5});
-//   test.insert(3, {7, 2, 3, 4, 5});
-//   test.insert(4, {7, 2, 3, 4, 5}); // Cannot insert more than MAX DEGREE of chunk
-//   std::cout << test << std::endl;
-//   auto dims = test.size();
-//   REQUIRE(dims.first == 4);
-//   REQUIRE(dims.second == 5);
-// }
+int main() {
 
-// TEST_CASE("Testing Leaf Chunk Order After Insertion", "[leaf_chunk]") {
-  
-// }
+  // Testing leaf chunk
+  LeafChunk test(5);
+  test.insert(1, {1, 2, 3, 4, 5});
+  test.insert(2, {3, 2, 3, 4, 5});
+  test.insert(1, {1, 2, 3, 4, 5});
+  test.insert(2, {3, 2, 3, 4, 5});
+  // TODO: Fix split functionality
+  bool should_split = test.insert(4, {4, 2, 3, 4, 5});
+  std::cout << should_split << std::endl;
+  std::cout << test.size().first << std::endl;
+  std::cout << test << std::endl;
+  test.split();
+  std::cout << test << std::endl;
 
-// TEST_CASE("Testing Leaf Chunk Insertion Duplicate Key", "[leaf_chunk]") {
+  return 0;
 
-// }
-
-// TEST_CASE("Testing Leaf Chunk After Split", "[leaf_chunk]") {
-
-// }
-
-// int main() {
-
-//   // Testing leaf chunk
-//   LeafChunk test(5);
-//   test.insert(1, {1, 2, 3, 4, 5});
-//   test.insert(2, {3, 2, 3, 4, 5});
-//   bool should_split = test.insert(4, {4, 2, 3, 4, 5});
-//   std::cout << should_split << std::endl;
-//   std::cout << test << std::endl;
-//   test.split();
-//   std::cout << test << std::endl;
-
-//   return 0;
-// }
+}
