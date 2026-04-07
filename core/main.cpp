204f31b5c610d074a4bd762922d49f59c775723b
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2026-04-07 01:46:00 -0400

    created test for split

--- core/main.cpp
+++ core/main.cpp
@@ -10,6 +10,7 @@ int main() {
   test->insert(2, {3, 2, 3, 4, 5});
   test->insert(1, {1, 2, 3, 4, 5});
   test->insert(2, {3, 2, 3, 4, 5});
+  test->insert(2, {3, 2, 3, 4, 5});
   // TODO: Fix split functionality
   // bool should_split = test->insert(4, {4, 2, 3, 4, 5});
   // std::cout << should_split << std::endl;
--- core/tests/chunks/test_leaf_chunk.cpp
+++ core/tests/chunks/test_leaf_chunk.cpp
@@ -9,7 +9,6 @@ TEST_CASE("Testing Leaf Chunk Insertion", "[leaf_chunk]") {
   test.insert(2, {3, 2, 3, 4, 5});
   test.insert(3, {7, 2, 3, 4, 5});
   test.insert(4, {7, 2, 3, 4, 5}); // Cannot insert more than MAX DEGREE of chunk
-  std::cout << test << std::endl;
   auto dims = test.size();
   REQUIRE(dims.first == 4);
   REQUIRE(dims.second == 5);
@@ -23,6 +22,14 @@ TEST_CASE("Testing Leaf Chunk Insertion", "[leaf_chunk]") {
 
 // }
 
-// TEST_CASE("Testing Leaf Chunk After Split", "[leaf_chunk]") {
-
-// }
+TEST_CASE("Testing Leaf Chunk After Split", "[leaf_chunk]") {
+  LeafChunk* test = new LeafChunk(5);
+  test->insert(1, {1, 2, 3, 4, 5});
+  test->insert(2, {3, 2, 3, 4, 5});
+  test->insert(3, {1, 2, 3, 4, 5});
+  test->insert(4, {3, 2, 3, 4, 5});
+  test->insert(5, {1, 2, 3, 4, 5});
+  std::pair<Chunk*, Chunk*> result = test->split();
+  REQUIRE(static_cast<LeafChunk*>(result.first)->size().first == 2);
+  REQUIRE(static_cast<LeafChunk*>(result.second)->size().first == 3);
+}
