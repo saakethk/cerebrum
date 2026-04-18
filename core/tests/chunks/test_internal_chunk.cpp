e09d778c5bb7a12f5249afc0b434ffc834ec2c1e
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2026-04-18 18:08:29 -0400

    commented out the test cases

--- core/tests/chunks/test_internal_chunk.cpp
+++ core/tests/chunks/test_internal_chunk.cpp
@@ -6,18 +6,18 @@
 #include "chunks/leaf_chunk.hpp"
 #include "chunks/internal_chunk.hpp"
 
-TEST_CASE("Testing Internal Chunk Insertion and Leaf Chunk Insertion", "[leaf_chunk]") {
-  Chunk* curr = new LeafChunk(5);
+// TEST_CASE("Testing Internal Chunk Insertion and Leaf Chunk Insertion", "[leaf_chunk]") {
+//   Chunk* curr = new LeafChunk(5);
 
-  std::stack<Chunk*> path;
-  while (curr->isLeaf() == false) {
-    // We know it is a Internal node if not leaf
-    curr = static_cast<InternalChunk*>(curr)->findNextChunk(key);
-  }
+//   std::stack<Chunk*> path;
+//   while (curr->isLeaf() == false) {
+//     // We know it is a Internal node if not leaf
+//     curr = static_cast<InternalChunk*>(curr)->findNextChunk(key);
+//   }
  
-  // auto dims = test->size();
-  // REQUIRE(dims.first == Chunk::MAX_DEGREE);
-  // REQUIRE(dims.second == 5);
+//   // auto dims = test->size();
+//   // REQUIRE(dims.first == Chunk::MAX_DEGREE);
+//   // REQUIRE(dims.second == 5);
 
-  // delete test;
-}
+//   // delete test;
+// }
