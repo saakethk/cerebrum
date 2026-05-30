f70193fc27fe02c183881e4cdef3e727653fd97f
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2026-05-30 17:33:35 -0400

    finished basic internal and leaf chunk unit tests; all pass

--- core/tests/chunks/test_internal_chunk.cpp
+++ core/tests/chunks/test_internal_chunk.cpp
@@ -4,7 +4,6 @@
 #include "catch_amalgamated.hpp"
 #include "chunks/internal_chunk.hpp"
 
-const unsigned int NUM_ATTRIBUTES = 1;
 const unsigned int CHUNK_SIZE = 10;
 const unsigned int NUM_INSERT = 5; // must be less than chunk size
 
@@ -24,7 +23,6 @@ TEST_CASE("Testing Internal Chunk", "[internal_chunk][basic]") {
 
   SECTION("Testing Valid Insertion") {
 
-    // testing insert
     c->insertChild(dummy);
 
     for (unsigned int j = 0; j < NUM_INSERT; j++) {
@@ -39,6 +37,8 @@ TEST_CASE("Testing Internal Chunk", "[internal_chunk][basic]") {
 
   SECTION("Testing Invalid Insertion") {
 
+    c->insertChild(dummy);
+
     for (unsigned int j = 0; j < CHUNK_SIZE; j++) {
       REQUIRE(c->insert(j, dummy) == Success);
     }
@@ -48,77 +48,86 @@ TEST_CASE("Testing Internal Chunk", "[internal_chunk][basic]") {
     }
 
     for (unsigned int j = CHUNK_SIZE; j < (CHUNK_SIZE + CHUNK_SIZE); j++) {
-      REQUIRE(leaf->insert(j, dummy_row) == Full);
+      REQUIRE(c->insert(j, dummy) == Full);
     }
 
-    REQUIRE(leaf->getNumItems() == CHUNK_SIZE);
+    REQUIRE(c->getNumItems() == CHUNK_SIZE);
 
 
   }
 
   SECTION("Testing Key Insertion Order") {
 
+    c->insertChild(dummy);
+
     for (unsigned int i = NUM_INSERT; i > 0 ; i--) {
       // i is key
-      REQUIRE(leaf->insert(i, dummy_row) == Success);
+      REQUIRE(c->insert(i, dummy) == Success);
     }
     
     for (unsigned int j = 0; j < NUM_INSERT; j++) {
       // j is offset here
-      REQUIRE(leaf->getKey(j) == j + 1);
+      REQUIRE(c->getKey(j) == j + 1);
     }
   }
 
   SECTION("Testing Chunk Splitting") {
 
-    // testing chunk splitting
+    c->insertChild(dummy);
 
     for (unsigned int j = 0; j < CHUNK_SIZE; j++) {
-      REQUIRE(leaf->insert(j, dummy_row) == Success);
+      REQUIRE(c->insert(j, dummy) == Success);
+      REQUIRE(c->getNext(j) == dummy);
     }
 
-    SplitChunk res = leaf->split();
-    LeafChunk* right_chunk = static_cast<LeafChunk*>(res.chunk);
+    REQUIRE(c->getNumItems() == CHUNK_SIZE);
+
+    SplitChunk res = c->split();
+    InternalChunk* right_chunk = static_cast<InternalChunk*>(res.chunk);
     
-    REQUIRE((right_chunk->getNumItems() + leaf->getNumItems()) == CHUNK_SIZE);
-    REQUIRE(right_chunk->getNext() == nullptr);
-    REQUIRE(right_chunk->getPrevious() == leaf);
-    REQUIRE(leaf->getNext() == right_chunk);
-    REQUIRE(leaf->getPrevious() == nullptr);
+    // checking chunk size - 1 because the split key should be moved up
+    REQUIRE((right_chunk->getNumItems() + c->getNumItems()) == (CHUNK_SIZE - 1));
 
   }
 
   SECTION("Testing Filling Chunk") {
 
+    c->insertChild(dummy);
+
     for (unsigned int j = 0; j < CHUNK_SIZE; j++) {
-      CHECK(leaf->insert(j, dummy_row) == Success);
+      CHECK(c->insert(j, dummy) == Success);
     }
 
-    REQUIRE(leaf->isFull() == true);
+    REQUIRE(c->isFull() == true);
 
   }
 
   SECTION("Testing Valid Deletion") {
 
+    c->insertChild(dummy);
+
     for (unsigned int j = 0; j < NUM_INSERT; j++) {
-      REQUIRE(leaf->insert(j, dummy_row) == Success);
-      REQUIRE(leaf->exists(j) == true);
-      REQUIRE(leaf->remove(j) == true);
+      REQUIRE(c->insert(j, dummy) == Success);
+      REQUIRE(c->exists(j) == true);
+      REQUIRE(c->remove(j) == true);
     }
 
-    REQUIRE(leaf->getNumItems() == 0);
+    REQUIRE(c->getNumItems() == 0);
   }
 
   SECTION("Testing Invalid Deletion") {
 
+    c->insertChild(dummy);
+
     for (unsigned int j = 0; j < NUM_INSERT; j++) {
-      REQUIRE(leaf->exists(j) == false);
-      REQUIRE(leaf->remove(j) == false);
+      REQUIRE(c->exists(j) == false);
+      REQUIRE(c->remove(j) == false);
     }
 
-    REQUIRE(leaf->getNumItems() == 0);
+    REQUIRE(c->getNumItems() == 0);
   }  
 
-  delete leaf;
+  delete c;
+  delete dummy;
   
 }
